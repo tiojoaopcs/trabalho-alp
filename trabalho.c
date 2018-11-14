69bdbfd7cd98b39189de8357bfdac93ad4cfa947
tiojoaopcs <44969719+tiojoaopcs@users.noreply.github.com>
2018-11-14 16:08:32 -0200

    Não está usando arquivos
    Comentei as partes de arquivos

--- trabalho.c
+++ trabalho.c
@@ -8,6 +8,8 @@
  */
 
 #include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #define len 201
 
 int i;
@@ -30,22 +32,60 @@ int comecaCom(char *str, char *proc){
     return 1;            
 }
 
-void ordena(ATOM **lista){
-    ATOM *aux = *lista;
+int comparar(ATOM *s1,ATOM *s2){
+    int ret=strcmp(s1->nome,s2->nome);
+    if(ret==0){
+        return strcmp(s1->amino,s2->amino);
+    }
+    else{
+        return ret;
+    }
+}
+
+void ordena(ATOM **ptrInic){
+    ATOM *novo=NULL;
+    ATOM *velho=*ptrInic;
+    while(velho!=NULL){
+        ATOM *maior=velho;
+        ATOM *atual=velho;
+        ATOM *ant,*antMaior;
+        while(13){
+            ant=atual;
+            atual=atual->prox;
+            if(atual==NULL){
+                break;
+            }
+            else{
+                if(comparar(atual,maior)>0){
+                    maior=atual;
+                    antMaior=ant;
+                }
+            }
+        }
+        if(velho==maior){
+            velho=velho->prox;
+        }
+        else{
+            antMaior->prox=maior->prox;
+        }
+        ATOM *segMaior=novo;
+        novo=maior;
+        novo->prox=segMaior;
+    }
+    *ptrInic=novo;
 }
 
 int main(int argc, char **argv){
     char linhas[100][len];
     int lidas=0;
     char candidato[len];    
-    FILE *arq = fopen(argv[1], "r+"), *saida = fopen(argv[2], "w+");
+    // FILE *arq = fopen(argv[1], "r+"), *saida = fopen(argv[2], "w+");
     
-    while(fgets(candidato, len, arq)){
-        printf("while rodando\n");
+    while(/* fgets(candidato, len, arq) */gets(candidato)){
         if(comecaCom(candidato,"ATOM")){
             strcpy(linhas[lidas],candidato+4);
             lidas++;
-        }                        
+        }                   
     }
 
     printf("Linhas lidas: %d\n",lidas);
@@ -54,11 +94,18 @@ int main(int argc, char **argv){
 
     for(i=0; i<lidas; i++){
         ATOM *new=malloc(sizeof(ATOM));
-        sscanf(linhas[i]," %d %s %s %*c %d %lf %lf %lf", new->id,new->nome,new->amino,new->tipo,new->x,new->y,new->z);
+        sscanf(linhas[i]," %d %s %s %*c %d %lf %lf %lf", &new->id,new->nome,new->amino,&new->tipo,&new->x,&new->y,&new->z);
         new->prox=inic;
         inic=new;
-    }    
-    fclose(arq);
-    fclose(saida);
+    }
+    ordena(&inic);
+    
+    ATOM *iter;
+    for(iter=inic; iter!=NULL; iter=iter->prox){
+        printf("Nome: %-6sAmino: %-6s\n",iter->nome,iter->amino);
+    }
+    
+    /* fclose(arq);
+    fclose(saida); */
     return 0;
 }
