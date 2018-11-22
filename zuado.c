71fa40cd8bd6f6964048cc0a7a5fa0cd8537c932
tiojoaopcs <44969719+tiojoaopcs@users.noreply.github.com>
2018-11-22 21:33:24 -0200

    Update dia 22 (mostrar 'quantidade' ta meio bugado)

--- zuado.c
+++ zuado.c
@@ -95,6 +95,13 @@ ATOM *querySelector(ATOM *inic,void *val,seletor cmp){
     }
 }
 
+//FUNÇÃO PARA IMPRIMIR
+void impressora(ATOM *lista){
+    for(lista; lista!=NULL; lista=lista->prox){
+        printf("Nome: %-6sAminoacido: %-6sTipo: %-6dID: %-6d\n", lista->nome, lista->amino, lista->tipo, lista->id);
+    }
+}
+
 int main(int argc, char **argv){
     char linhas[100][len];
     int lidas=0;
@@ -124,16 +131,19 @@ int main(int argc, char **argv){
 
     //Menu
     printf("\n=======================================\n");
-    printf("Digite uma opção dentre as seguintes:\n");
-    printf("\"listar (nome/numero)\" - Listar átomos. O nome ou número do átomo são opcionais.\n");
-    printf("\"gerar\" - Gerar arquivo de saída.\n");
-    printf("\"quantidade (tipo/amino)\" - Mostra a quantidade de átomos de cada tipo ou a quantidade de aminoácidos de cada tipo.\n");
-    printf("\"total\" - Exibe a quantidade total de aminoácidos no arquivo.\n");
+    printf("Digite uma opcao dentre as seguintes:\n");
+    printf("\"listar (nome/numero)\" - Listar atomos. O nome ou numero do atomo sao opcionais.\n");
+    printf("\"gerar\" - Gerar arquivo de saida.\n");
+    printf("\"quantidade (nome X/amino XYZ)\" - Mostra a quantidade de atomos de cada tipo ou a quantidade de aminoacidos de cada tipo.\n");
+    printf("\"total\" - Exibe a quantidade total de aminoacidos no arquivo.\n");
     printf("\"encerrar\" - Encerra\n");
     printf("=======================================\n\n");
 
     char m[2][len];
-    while("17xXMeuPartidoÉOBrasilXx17"){
+
+    int contador; //será utilizado quando a "quantidade" for solicitada
+
+    while("1"){
         strcpy(m[0],"");
         strcpy(m[1],"");
         char sep;
@@ -148,23 +158,23 @@ int main(int argc, char **argv){
                 l++;
             }
         }
-
+        printf("\n");
         if(strcmp(m[0], "listar")==0){
             if(l==0){
-                ATOM *iter;
-                for(iter=inic; iter!=NULL; iter=iter->prox){
-                    printf("Nome: %-6sAmino: %-6sTipo: %-6d\n",iter->nome,iter->amino,iter->tipo);
-                }
+                ATOM *aux;
+                aux = inic;
+                ordena(&aux);
+                impressora(aux);
             }
             else{
                 if(isdigit(m[1][0])){
                     int num=atoi(m[1]);
                     ATOM *teste=querySelector(inic,&num,seletorId);
                     if(teste==NULL){
-                        printf("deu nao fi rsrs\n");//faze funçao pra imprimir
+                        printf("Nao foi possivel encontrar o ID %s\n", m[1]);//faze funçao pra imprimir
                     }
                     else{
-                        printf("deu sim fi rsrs\n");
+                        printf("Nome: %-6sAminoacido: %-6sTipo: %-6dID: %-6d\n", teste->nome, teste->amino, teste->tipo, teste->id);
                     }
                 }
                 else{
@@ -173,11 +183,11 @@ int main(int argc, char **argv){
                     while(42){
                         teste2=querySelector(teste2,m[1],seletorNome);
                         if(teste2==NULL){
-                            if(!deu) printf("deu nao fi rsrs\n");
+                            if(!deu) printf("Nao foi possivel listar os atomos com o nome \"%s\"\n", m[1]);
                             break;
                         }
                         else{
-                            printf("TESTE2 = Id: %-6dX: %-6.3lf\n",teste2->id,teste2->x);
+                            printf("Nome: %-6sAminoacido: %-6sTipo: %-6dID: %-6d\n", teste2->nome, teste2->amino, teste2->tipo, teste2->id);
                             deu=1;
                             teste2=teste2->prox;
                         }
@@ -194,15 +204,33 @@ int main(int argc, char **argv){
         }
 
         else if(strcmp(m[0], "quantidade") == 0){
-            if(l == 0){
-                printf("hey gringo do you want a kick in the saco asshole rsrs\n");
+            if(l <= 1){
+                printf("Poucos argumentos para listar a quantidade. Digite \"quantidade + nome + [nome]\" ou \"quantidade + amino + [aminoacido]\"\n");
             }
             else{
-                if(strcmp(m[1], "tipo") == 0){
-
+                if(strcmp(m[1], "nome") == 0){
+                    contador = 0;
+                    printf("teste inic = %p\n", inic);
+                    ATOM *qtd = querySelector(inic, m[2], seletorNome);
+                    printf("teste\n");
+                    if(qtd == NULL) printf("Nao foi possivel encontrar \"%s\"\n", m[2]);
+                    else{
+                        impressora(qtd);
+                        printf("teste\n");
+                        while(qtd){
+                            qtd=qtd->prox;
+                            contador++;
+                            printf("contador = %d\n", contador);
+                        }
+                        printf("Ha %d atomos com o nome %s\n", contador, m[2]);
+                    }
                 }
             }
         }
+        else if(strcmp(m[0], "encerrar") == 0){
+            return;
+        }
+        printf("\n");
     }
     //***************************************************
 
@@ -232,3 +260,18 @@ int main(int argc, char **argv){
     fclose(saida); */
     return 0;
 }
+
+/*
+ENTRADA
+
+SCALE1      0.066667  0.000000  0.028849        0.00000
+SCALE2      0.000000  0.050505  0.000000        0.00000
+SCALE3      0.000000  0.000000  0.066037        0.00000
+ATOM      1  N   GLY A   1      -4.788  -8.935   3.453  1.00 11.53           N
+ATOM      2  CA  GLY A   1      -4.218 -10.294   3.312  1.00  9.54           C
+ATOM      3  C   GLY A   1      -3.815 -10.534   1.870  1.00  8.53           C
+ATOM      4  O   GLY A   1      -4.276  -9.836   0.965  1.00  7.01           O
+ATOM      5  N   CYS A   2      -3.045 -11.594   1.654  1.00  7.14           N
+ATOM      6  CA  CYS A   2      -2.531 -11.945   0.337  1.00  7.39           C
+ATOM      7  C   CYS A   2      -3.485 -11.922  -0.844  1.00  7.12           C
+*/
